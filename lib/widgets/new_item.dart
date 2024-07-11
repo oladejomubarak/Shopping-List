14a6791156ecf9cf1d09adb629c77dd572fa66b9
oladejomubarak <oladejomubarakade@gmail.com>
2024-07-11 16:52:48 +0100

    added validation for input

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -10,6 +10,11 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +24,7 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
@@ -27,7 +33,13 @@ class _NewItemState extends State<NewItem> {
                   label: Text("Name"),
                 ),
                 validator: (value) {
-                  return "Demo...";
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return "Must be between 1 and 50 characters";
+                  }
+                  return null;
                 },
               ),
               Row(
@@ -39,6 +51,16 @@ class _NewItemState extends State<NewItem> {
                         label: Text("Quantity"),
                       ),
                       initialValue: "1",
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return "Must be a valid positive number ";
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   const SizedBox(
@@ -64,6 +86,24 @@ class _NewItemState extends State<NewItem> {
                             ))
                     ], onChanged: (value) {}),
                   ),
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: const Text("Reset"),
+                  ),
+                  ElevatedButton(
+                    onPressed: _saveItem,
+                    child: const Text("Add Item"),
+                  )
                 ],
               )
             ],
